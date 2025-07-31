cb413c910a1891a8382b327d6665c4eb2d931db1
Max <0xmaxyz@gmail.com>
2025-07-31 10:39:40 +0330

    Add basic settler

---
+++ contracts/src/settler/MarginSettler.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {Address, AddressLib} from "@1inch/solidity-utils/libraries/AddressLib.sol";
+import {SafeERC20} from "@1inch/solidity-utils/libraries/SafeERC20.sol";
+import {UniERC20} from "@1inch/solidity-utils/libraries/UniERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+import {IOrderMixin} from "@1inch/lo/interfaces/IOrderMixin.sol";
+import {IPreInteraction} from "@1inch/lo/interfaces/IPreInteraction.sol";
+import {IPostInteraction} from "@1inch/lo/interfaces/IPostInteraction.sol";
+import {ITakerInteraction} from "@1inch/lo/interfaces/ITakerInteraction.sol";
+import {MakerTraits, MakerTraitsLib} from "@1inch/lo/libraries/MakerTraitsLib.sol";
+import {ContractSigner} from "../ContractSigner.sol";
+import "../Errors.sol";
+import {ComposerCommands} from "./enums/DeltaEnums.sol";
+import {Composer} from "../settler/Composer.sol";
+
+contract MarginSettler is
+    IPreInteraction,
+    IPostInteraction,
+    ITakerInteraction,
+    ContractSigner,
+    Composer
+{
+    using AddressLib for Address;
+    using SafeERC20 for IERC20;
+    using UniERC20 for IERC20;
+    using Math for uint256;
+    using MakerTraitsLib for MakerTraits;
+
+    address private immutable _LIMIT_ORDER_PROTOCOL;
+    address private immutable _WETH;
+
+    constructor(address limitOrderProtocol, address weth) {
+        _LIMIT_ORDER_PROTOCOL = limitOrderProtocol;
+        _WETH = weth;
+    }
+
+    modifier onlyLimitOrderProtocol() {
+        if (msg.sender != _LIMIT_ORDER_PROTOCOL)
+            revert OnlyLimitOrderProtocol();
+        _;
+    }
+
+    function preInteraction(
+        IOrderMixin.Order calldata order,
+        bytes calldata extension,
+        bytes32 orderHash,
+        address taker,
+        uint256 makingAmount,
+        uint256 takingAmount,
+        uint256 remainingMakingAmount,
+        bytes calldata extraData
+    ) external override onlyLimitOrderProtocol {
+        address user = order.receiver.get();
+        uint256 amount = order.makingAmount.get();
+        _lendingParser(user, amount, extraData);
+    }
+
+    function _lendingParser(
+        address user,
+        uint256 amount,
+        bytes calldata lendingOps
+    ) internal {
+        uint256 length;
+        uint256 maxIndex;
+        uint256 currentOffset;
+        assembly {
+            length := calldataload(add(lendingOps.offset, 0x20))
+            maxIndex := add(length, lendingOps.offset)
+            currentOffset := add(lendingOps.offset, 0x40)
+        }
+
+        while (true) {
+            uint256 operation;
+            // fetch op metadata
+            assembly {
+                operation := shr(248, calldataload(currentOffset)) // last byte
+                // we increment the current offset to skip the operation
+                currentOffset := add(1, currentOffset)
+            }
+            if (operation == ComposerCommands.LENDING) {
+                currentOffset = _lendingOperations(
+                    callerAddress,
+                    currentOffset,
+                    amount
+                );
+            } else if (operation == ComposerCommands.TRANSFERS) {
+                currentOffset = _transfers(
+                    currentOffset,
+                    callerAddress,
+                    amount
+                );
+            } else if (operation == ComposerCommands.PERMIT) {
+                currentOffset = _permit(currentOffset, callerAddress, amount);
+            } else {
+                _invalidOperation();
+            }
+            // break if we skipped over the calldata
+            if (currentOffset >= maxIndex) break;
+        }
+        // revert if some excess is left
+        if (currentOffset > maxIndex) revert InvalidCalldata();
+    }
+
+    /// @notice Post-interaction: Handle any cleanup after all transfers
+    function postInteraction(
+        IOrderMixin.Order calldata order,
+        bytes calldata extension,
+        bytes32 orderHash,
+        address taker,
+        uint256 makingAmount,
+        uint256 takingAmount,
+        uint256 remainingMakingAmount,
+        bytes calldata extraData
+    ) external override onlyLimitOrderProtocol {
+        // TODO
+    }
+
+    /// @notice Taker interaction: Handle intermediate logic between transfers
+    function takerInteraction(
+        IOrderMixin.Order calldata order,
+        bytes calldata extension,
+        bytes32 orderHash,
+        address taker,
+        uint256 makingAmount,
+        uint256 takingAmount,
+        uint256 remainingMakingAmount,
+        bytes calldata extraData
+    ) external override onlyLimitOrderProtocol {
+        // TODO
+    }
+
+    function takeOrder(
+        IOrderMixin.Order calldata order,
+        bytes calldata signature,
+        uint256 amount,
+        TakerTraits takerTraits,
+        bytes calldata args
+    )
+        external
+        returns (uint256 makingAmount, uint256 takingAmount, bytes32 orderHash)
+    {
+        return
+            IOrderMixin(_LIMIT_ORDER_PROTOCOL).fillContractOrderArgs(
+                order,
+                signature,
+                amount,
+                takerTraits,
+                args
+            );
+    }
+}
